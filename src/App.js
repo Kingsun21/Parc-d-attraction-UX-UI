937b8ad4d03ef3d43a00b603d72d77267819320e
Kingsun21 <t.grimault21@gmail.com>
2018-10-19 14:37:09 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+import { Media } from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar color="light" light expand="md">
+          <img src="/starfpark.png" alt="starfpark" />
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/attractions/">Attractions</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/batiments/">Bâtiment</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/maintenance/">Maintenance</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/personnel/">Personnel</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/stats/">Statistiques</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
