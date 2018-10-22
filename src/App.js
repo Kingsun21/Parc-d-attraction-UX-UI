e0689db1a23cb1764b2dcf2e08f246b3a1b87ad8
alexandre282 <fontaniea@yahoo.fr>
2018-10-22 22:32:52 +0200

    hi

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-#oui je vais un tes
 
 import {
   Collapse,
